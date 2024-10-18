54ac7b32518fb5fe96d9f228cf0e8d0bdc04f226
kkatiko <k7a8t0e9@gmail.com>
2024-10-18 22:25:11 +0300

    Update find_min_max.c

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -1,12 +1,23 @@
-#include "find_min_max.h"
+#include "find_min_max.h" 
+#include <limits.h> 
 
-#include <limits.h>
+struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) { 
+  struct MinMax min_max; 
+  min_max.min = INT_MAX; 
+  min_max.max = INT_MIN; 
 
-struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
-  struct MinMax min_max;
-  min_max.min = INT_MAX;
-  min_max.max = INT_MIN;
+  // Проходим по всем элементам массива в заданном промежутке
+  for (unsigned int i = begin; i < end; i++) {
+    // Обновляем минимальное значение, если текущий элемент меньше
+    if (array[i] < min_max.min) {
+      min_max.min = array[i];
+    }
 
-  // your code here
-  return min_max;
-}
+    // Обновляем максимальное значение, если текущий элемент больше
+    if (array[i] > min_max.max) {
+      min_max.max = array[i];
+    }
+  }
+
+  return min_max; 
+} 
